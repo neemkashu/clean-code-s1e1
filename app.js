dce37511d8a5d7593d7ab3b9579837ae6c42e30a
neemkashu <sparjinkust@mail.ru>
2022-10-28 14:32:36 +0300

    refactor: replace tag selector input with BEM-styled class according to rules 3.1, 3.2, 3.3, and 3.4 in html-and-css.md and rule 2.1 html-and-css-extended.md

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ var createNewTaskElement = function(taskString){
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
+    checkBox.className = "task__check";
+
     editInput.type = "text";
     editInput.className = "input-field";
     editInput.classList.add("task__input");
@@ -167,7 +169,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents = function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox = taskListItem.querySelector("input[type = checkbox]");
+    var checkBox = taskListItem.querySelector(".task__check");
     var editButton = taskListItem.querySelector("button.edit");
     var deleteButton = taskListItem.querySelector("button.delete");
 
